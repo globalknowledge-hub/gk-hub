bd43a983361d59dfda89c439aa7fdac3d8b4c6c2
hello@horaizontech.xyz <hello@horaizontech.xyz>
2026-04-02 21:09:01 +0530

    fix: disable middleware invocation completely by using empty matcher

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-  matcher: ['/((?!_next|api|static).*)'],
-};
-
 export function middleware(_req: NextRequest) {
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [],  // Empty matcher - disable middleware
+};
